d2f5fc02f2bbd0f0498ef67dfaa9f97ae09e2cda
Lucian Buzzo <lucian@gravitywell.co.uk>
2016-12-26 09:08:24 +0000

    Switched to using vectors for maze generation, using VictorJS

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const DungeonFactory = require('./lib/generator.js');
 let dungeon = DungeonFactory.generate({
   width: 41,
   height: 41
-});
+}, true);
 
 dungeon.tiles.forEach(column => {
   let columnText = column.map(t => {
