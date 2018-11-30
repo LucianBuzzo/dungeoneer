b19738f1ca7138709a6bafff2d1518f7fc650812
Lucian <lucian.buzzo@gmail.com>
2018-11-30 22:55:56 +0000

    Add standardJS linting and use it to fix linting errors
    Change-type: patch
    Signed-off-by: Lucian <lucian.buzzo@gmail.com>

--- test.js
+++ test.js
@@ -1,22 +1,20 @@
-const DungeonFactory = require('./lib/generator.js');
+const DungeonFactory = require('./lib/generator.js')
 
 let dungeon = DungeonFactory.generate({
   width: 41,
   height: 41
-}, true);
+}, true)
 
 dungeon.tiles.forEach(column => {
   let columnText = column.map(t => {
     if (t.type === 'wall') {
-      return '█';
+      return '█'
     }
     if (t.type === 'door') {
-      return '+';
+      return '+'
     }
-    return '.';
-  }).join('');
+    return '.'
+  }).join('')
 
-  console.log(columnText);
-});
-
-
+  console.log(columnText)
+})
--- web/main.js
+++ web/main.js
@@ -1,48 +1,49 @@
-var canvas = document.querySelector('canvas');
-var ctx = canvas.getContext('2d');
+/* global Dungeoneer */
+var canvas = document.querySelector('canvas')
+var ctx = canvas.getContext('2d')
 
-ctx.imageSmoothingEnabled = false;
+ctx.imageSmoothingEnabled = false
 
-var create = function(width, height) {
-  var cellSize = 4;
+var create = function (width, height) {
+  var cellSize = 4
   var dungeon = Dungeoneer.generate({
     width: width,
     height: height
-  });
+  })
 
-  canvas.width = width * cellSize;
-  canvas.height = height * cellSize;
+  canvas.width = width * cellSize
+  canvas.height = height * cellSize
 
-  canvas.style.width = width * cellSize + 'px';
-  canvas.style.height = height * cellSize + 'px';
+  canvas.style.width = width * cellSize + 'px'
+  canvas.style.height = height * cellSize + 'px'
 
-  ctx.fillStyle = 'black';
-  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.fillStyle = 'black'
+  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
 
-  ctx.fillStyle = 'red';
+  ctx.fillStyle = 'red'
 
   dungeon.rooms.forEach((room) => {
-    ctx.fillStyle = 'red';
-    ctx.fillRect(room.x * cellSize, room.y * cellSize, room.width * cellSize, room.height * cellSize);
-  });
+    ctx.fillStyle = 'red'
+    ctx.fillRect(room.x * cellSize, room.y * cellSize, room.width * cellSize, room.height * cellSize)
+  })
 
-  ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
+  ctx.fillStyle = 'rgba(255, 255, 255, 0.5)'
   for (var i = 0; i < dungeon.tiles.length; i++) {
     for (var j = 0; j < dungeon.tiles.length; j++) {
       if (dungeon.tiles[i][j].type === 'floor') {
-        ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
-        ctx.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
+        ctx.fillStyle = 'rgba(255, 255, 255, 0.5)'
+        ctx.fillRect(i * cellSize, j * cellSize, cellSize, cellSize)
       }
       if (dungeon.tiles[i][j].type === 'door') {
-        ctx.fillStyle = 'yellow';
-        ctx.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
+        ctx.fillStyle = 'yellow'
+        ctx.fillRect(i * cellSize, j * cellSize, cellSize, cellSize)
       }
     }
   }
-};
+}
 
-document.querySelector('button').addEventListener('click', function() {
-  create(51, 51);
-}, false);
+document.querySelector('button').addEventListener('click', function () {
+  create(51, 51)
+}, false)
 
-create(51, 51);
+create(51, 51)
