67b13fe1c131c7bd8c35e6494a4497a28ce5dd60
Lucian <lucian.buzzo@gmail.com>
2018-12-01 23:06:37 +0000

    Add test for width and height of tiles array
    Change-type: patch
    Signed-off-by: Lucian <lucian.buzzo@gmail.com>

--- test/index.spec.js
+++ test/index.spec.js
@@ -18,3 +18,19 @@ ava.test('.build() should return an object containing the key "rooms"', (test) =
 
   test.true(dungeon.hasOwnProperty('rooms'))
 })
+
+ava.test('.build() should return an 2d array of tiles proportional to the width and height options', (test) => {
+  const width = 21
+  const height = 31
+
+  const dungeon = dungeoneer.build({
+    width,
+    height
+  })
+
+  test.is(dungeon.tiles.length, width)
+
+  for (const column of dungeon.tiles) {
+    test.is(column.length, height)
+  }
+})
