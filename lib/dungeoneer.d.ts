33afec58578eb5f43d4da669ae6082f2b3fb348a
Michael Pearson <mipearson@gmail.com>
2019-12-28 13:17:00 +1100

    Fix the dungoneer.d.ts
    change-type: patch

--- lib/dungeoneer.d.ts
+++ lib/dungeoneer.d.ts
@@ -1,5 +1,5 @@
-declare module 'dungeoneer' {
-  type Tile = {
+declare module "dungeoneer" {
+  type PlainTile = {
     // An object containing the tiles immediately surrounding this tile.
     nesw: {
       n?: Tile;
@@ -15,27 +15,41 @@ declare module 'dungeoneer' {
     y: number;
 
     // 'floor' and 'door' are passable terrain and a wall is impassable terrain.
-    type: 'wall' | 'floor' | 'door';
+    type: "wall" | "floor" | "door";
+  };
+
+  interface Tile extends PlainTile {
+    toJS(): PlainTile;
   }
 
-  type Room = {
+  type PlainRoom = {
     height: number;
     width: number;
     x: number;
     y: number;
+  };
+
+  interface Room extends PlainRoom {
+    getBoundingBox(): {
+      top: number;
+      right: number;
+      bottom: number;
+      left: number;
+    };
+    containsTile(x: number, y: number): boolean;
+    toJS(): PlainRoom;
+    intersects(other: Room): boolean;
   }
 
   type Dungeon = {
     rooms: Room[];
     tiles: Array<Tile[]>;
     seed: string | number;
-  }
+  };
 
-  function build = (options: {
+  function build(options: {
     width: number;
     height: number;
     seed?: string | number;
   }): Dungeon;
-
-  export = build;
 }
